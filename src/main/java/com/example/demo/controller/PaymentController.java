3bba0675d0c890fc2c3286884f25a6a9940ed38b
DELL <DELL@DESKTOP-E1F8288>
2024-01-13 17:55:07 +0530

    Version-1

--- src/main/java/com/example/demo/controller/PaymentController.java
+++ src/main/java/com/example/demo/controller/PaymentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.entities.Users;
@@ -13,6 +14,7 @@ import com.example.demo.services.UsersService;
 import com.razorpay.Order;
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
+import com.razorpay.Utils;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -26,7 +28,6 @@ public class PaymentController {
 		return "pay";
 	}
 	
-
 	@SuppressWarnings("finally")
 	@PostMapping("/createOrder")
 	@ResponseBody
@@ -55,6 +56,25 @@ public class PaymentController {
 		}
 		finally {
 			return order.toString();
+		}
+	}
+	@PostMapping("/verify")
+	@ResponseBody
+	public boolean verifyPayment(@RequestParam String orderId, @RequestParam String paymentId,@RequestParam String signature)
+	{
+		try
+		{
+			RazorpayClient razorpayClient = new RazorpayClient("rzp_test_7Z2eY0QTmz6eTV","XePzA994vvcU7v1ZlnI5kewf");
+			
+			String verificationData = orderId+"|"+paymentId;
+			boolean isValidSignature = Utils.verifySignature(verificationData, signature, "XePzA994vvcU7v1ZlnI5kewf");
+			return isValidSignature;
+			
+		}
+		catch(RazorpayException e)
+		{
+			e.printStackTrace();
+			return false;
 		}
 	}
 	
